ec1ce3d27f17ffbf4739a6baec4552bf067b4db3
Andrew Hornig <andrew.hornig@gmail.com>
2017-05-03 15:11:57 -0600

    added zip commands to census_data.py

--- census_data.py
+++ census_data.py
@@ -1,5 +1,11 @@
 import csv, numpy, matplotlib.pyplot as plt, plotly
 
+import zipfile
+zip_ref = zipfile.ZipFile('data.zip', 'r')
+zip_ref.extractall()
+zip_ref.close()
+
+
 #~~~~~~~~~~~~HOUSING DATA~~~~~~~~~~~~~~~~
 
 IDmeddata = {}
