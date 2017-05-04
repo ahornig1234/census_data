afcef8a014612ef91fc3d19c744f1a5486b2b597
Andrew Hornig <andrew.hornig@gmail.com>
2017-05-04 14:23:53 -0600

    trying to put local pics in again

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def homepage():
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
 
-    <img src="http://drive.google.com/file/d/0B2h-M49bJOxNRlcwa21FOWlDQ0U/view?usp=sharing" />
+    <img src="fig.pdf />
     """.format(time=the_time)
 
 if __name__ == '__main__':
