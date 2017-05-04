945d6d59fad8bbf6b02393cd6a43e380a35f7e63
Andrew Hornig <andrew.hornig@gmail.com>
2017-05-04 14:33:13 -0600

    trying to put local pics in

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+#    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
 
-    <img src="fig.pdf">
+    <img src="/Users/ahornig/Documents/Research/Programs/github/census_data/fig.pdf">
     """.format(time=the_time)
 
 if __name__ == '__main__':
